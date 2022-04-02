38c6148df83a2f49d55835dc24199f6dc3adcd52
Rosa <ygao112@jhu.edu>
2022-04-01 20:55:38 -0400

    add faker to populate data

--- server/index.js
+++ server/index.js
@@ -1,4 +1,17 @@
 const express = require("express");
+const faker = require("faker");
+const NoteDao = require("./data/NoteDao");
+
+const NUM_SAMPLES = 3;
+const notes = new NoteDao();
+for(let i = 0; i < NUM_SAMPLES; i++) {
+  notes.create({
+    title: faker.lorem.sentence(),
+    text: faker.lorem.paragraph(),
+  });
+}
+
+
 const app = express();
 const port = process.env.PORT || 3000;
 
