5a6988f92ff2d8469fb360c7f9f435a6298a8b18
Rosa <ygao112@jhu.edu>
2022-04-01 20:05:54 -0400

    create data access object for Note

---
+++ server/data/NoteDao.js
@@ -0,0 +1,29 @@
+const Note = require("../model/Note");
+
+class NoteDao {
+  constructor() {
+    this.note = [];
+  }
+  
+  async create({ title, text }){
+
+  }
+
+  async update( id, { title, text }){
+
+  }
+
+  async delete( id ) {
+
+  }
+
+  async read( id ) {
+
+  }
+
+  async readAll (query = "") {
+
+  }
+}
+
+module.exports = NoteDao;
