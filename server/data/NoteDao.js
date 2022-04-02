a5060a2d6b3fd686c39a6ffa18b67d5fcc6e61e5
Rosa <ygao112@jhu.edu>
2022-04-01 20:45:57 -0400

    implement error codes for NoteDao

--- server/data/NoteDao.js
+++ server/data/NoteDao.js
@@ -1,5 +1,5 @@
 const Note = require("../model/Note");
-const Note = require("../model/Note");
+const ApiError = require("../model/ApiError");
 
 class NoteDao {
   constructor() {
@@ -7,14 +7,27 @@ class NoteDao {
   }
 
   async create({ title, text }){
+    if (!title) {
+      throw new ApiError(400, "Note must have a valid title");
+    }
+    if (!text) {
+      throw new ApiError(400, "Note must have a valid text body");
+    }
+
     const newNote = new Note(title, text);
     this.note.push(newNote);
     return newNote;
   }
 
+
   async update( id, { title, text }){
 
     const index = this.note.findIndex((note)=>note._id === id);
+    
+    if (index === -1) {
+      throw new ApiError(404, "Given ID not found");
+    }
+
     if (title) {
       this.note[index].title = title;
     }
@@ -22,17 +35,20 @@ class NoteDao {
       this.note[index].text = text;
     }
     return this.note[index];
-
   }
 
   async delete( id ) {
     const index = this.note.findIndex((note)=>note._id === id);
+    if (index === -1) {
+      throw new ApiError(404, "Given ID not found");
+    }
     const note = this.note[index];
     this.note.splice(index, 1);
     return note;
 
   }
 
+  // return empty array if no matching note
   async read( id ) {
     const note = this.note.find((note)=>note._id === id);
     return note;
