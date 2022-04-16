3af01c272d729c8f1be46b024713d15b819a3d9f
Rosa <ygao112@jhu.edu>
2022-04-15 20:36:48 -0400

    create routes objects for users and notes

--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
 const express = require("express");
-const NoteDao = require("./data/NoteDao");
 const db = require("./data/db");
-
-const notes = new NoteDao();
+const notes = require("./routes/notes");
+const users = require("./routes/users");
 
 db.connect();
 
@@ -10,55 +9,12 @@ const app = express();
 app.use(express.json()); // allow Express to parse the request body
 const port = process.env.PORT || 3000;
 
+app.use(notes);
+app.use(users);
 
 app.get("/", (_request, response)=>{
-  response.send("api app!");
+  response.send("quick-note app api");
 });
-
-app.get("/api/notes", async (req, res) => {
-  const { query } = req.query;
-  const data = await notes.readAll(query);
-  res.json({ data });
-});
-
-app.get("/api/notes/:id", async (req, res)=>{
-  const { id } = req.params;
-  const data = await notes.read(id);
-  res.json({ data : data ? data : [] });
-});
-
-app.post("/api/notes", async (req, res)=>{
-  try {
-    const { title, text } = req.body;
-    const data = await notes.create({ title, text });
-    res.status(201).json({ data });
-    
-  } catch(err) {
-    res.status(err.status).json({ message: err.message });
-  }
-});
-
-app.delete("/api/notes/:id", async (req, res)=>{
-  try{
-    const { id } = req.params;
-    const data = await notes.delete(id);
-    res.status(200).json({ data });
-  } catch(err) {
-    res.status(err.status).json({ message: err.message });
-  }
-});
-
-app.put("/api/notes/:id", async (req,res)=>{
-  try{
-    const { id } = req.params;
-    const { title, text } = req.body;
-    const data = await notes.update(id, {title, text});
-    res.status(200).json({ data });
-  } catch(err) {
-    res.status(err.status).json({ message: err.message });
-  }
-})
-
 
 app.listen(port, ()=>{
   console.log(`Express app listening at port ${port}`);
